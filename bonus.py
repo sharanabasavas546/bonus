a072474060c1b4cf19c47719d09b49533e31c6ec
sharanabasavas546 <sharanabasavas546@gmail.com>
2025-11-10 16:24:07 +0530

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -1,12 +1,21 @@
-# salary_bonus.py
+import sys
 
-# Take input
-salary = float(input("Enter employee's salary: "))
+# Check if the correct number of arguments is provided
+if len(sys.argv) == 2:
+    script_name = sys.argv[0]
+    salary = float(sys.argv[1])
+    print("User provided input value:")
+else:
+    script_name = sys.argv[0]
+    salary = 50000.0
+    print("No input given - using default value:")
 
 # Calculate bonus
 bonus = 0.10 * salary
 total_salary = salary + bonus
 
 # Display results
+print(f"Script Name: {script_name}")
+print(f"Salary: ₹{salary:.2f}")
 print(f"Bonus amount: ₹{bonus:.2f}")
 print(f"Total salary after adding bonus: ₹{total_salary:.2f}")
